e066386566357e4bfc0e7155fc19d66137cadcfa
HyonWooKang <87benzenes@gmail.com>
2021-04-18 13:12:18 +0900

    Final

---
+++ TJ_mini/src/no1/game/minigame/run/Application.java
@@ -0,0 +1,13 @@
+package no1.game.minigame.run;
+
+import no1.game.minigame.layout.NumberGame;
+
+public class Application {
+	
+	public static void main(String[] args) {
+	
+	new NumberGame();
+	
+	}
+
+}
